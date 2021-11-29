7c08f925f3a7e9554fe2b12d4f404fb7f7861f96
TDL2627 <the.dark.lord.2627@gmail.com>
2021-11-29 11:31:38 +0200

    first upload

---
+++ java.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+	let button = document.querySelector("#btn");
+	button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+
+	let height = parseInt(document
+			.querySelector("#height").value);
+
+	
+	let weight = parseInt(document
+			.querySelector("#weight").value);
+
+	let result = document.querySelector("#result");
+
+
+	if (height === "" || isNaN(height))
+		result.innerHTML = "Provide a valid Height!";
+
+	else if (weight === "" || isNaN(weight))
+		result.innerHTML = "Provide a valid Weight!";
+
+	// If both input is valid, calculate the bmi
+	else {
+
+		// Fixing upto 2 decimal places
+		let bmi = (weight / ((height * height)
+							/ 10000)).toFixed(2);
+
+		
+		if (bmi < 18.6) result.innerHTML =
+			`Annorixic , just orders water : <span>${bmi}</span>`;
+
+		else if (bmi >= 18.6 && bmi < 24.9)
+			result.innerHTML =
+				`Fit,usually orders one big mac : <span>${bmi}</span>`;
+
+		else result.innerHTML =
+			`Obese,usually orders 5 big macs : <span>${bmi}</span>`;
+	}
+}
